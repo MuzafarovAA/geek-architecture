ebfdd0e58f9f7270d2939b5724072e36c384dae2
MuzafarovAA <artmuz@mail.ru>
2022-08-10 20:33:16 +0500

    Homework 2

--- geek-web-server/src/main/java/ru/geekbrains/ResponseSerializer.java
+++ geek-web-server/src/main/java/ru/geekbrains/ResponseSerializer.java
@@ -4,8 +4,16 @@ import ru.geekbrains.domain.HttpResponse;
 
 public class ResponseSerializer {
 
-    public String serialize(HttpResponse response) {
-        // TODO
-        return "";
+    public static String serialize(HttpResponse response) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("HTTP/1.1 ").append(response.getStatusCode());
+        if (response.getStatusCode() == 200) {
+            stringBuilder.append(" OK\n");
+        } else if (response.getStatusCode() == 404) {
+            stringBuilder.append(" NOT_FOUND\n");
+        }
+        response.getHeaders().forEach((k, v) -> stringBuilder.append(k).append(": ").append(v).append("\n"));
+        stringBuilder.append("\n").append(response.getBody());
+        return stringBuilder.toString();
     }
 }
